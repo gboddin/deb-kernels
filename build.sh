877143bf9b4910e51a0d7cead5ffc56d580442de
Gregory Boddin <gregory@siwhine.net>
2017-08-18 21:58:22 +0200

    Finalized workflow

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -x
 set -e
+REVISION=$(date +%s)
+[ ! -z "$1" ] && REVISION=$1
 apt-get update
 apt-get install --no-install-recommends -y ca-certificates wget build-essential kernel-package linux-image-amd64
 wget https://github.com/torvalds/linux/archive/master.tar.gz -O -|tar -xz
@@ -8,4 +10,4 @@ mv linux-master linux
 cp /boot/config* linux/.config
 cd linux
 yes '' | make oldconfig
-make-kpkg -j$(cat /proc/cpuinfo|grep -i bogo|wc -l) binary
+make-kpkg -j$(cat /proc/cpuinfo|grep -i bogo|wc -l) binary --append-to-version -master --revision ${REVISION} --initrd
